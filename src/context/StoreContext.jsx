5e61e9adfdbd5bac95f84aceea01793f0e027355
Zaid Dagbanbia <mohammed.dagbanbia@dci-student.org>
2024-08-18 19:59:12 +0200

    edit backend url

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -2,13 +2,12 @@ import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 
-
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-del-backend-5ytg.onrender.com";
+  const url ="https://food-del-backend-5ytg.onrender.com";
   const [token,setToken] = useState("");
   const [food_list, setFoodList] = useState([])
 
